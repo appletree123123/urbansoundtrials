e8e059facf506f37f452074e65cd7133743d2846
Perfetto_team <perfettomodularlaboratory@gmail.com>
2018-12-11 20:38:27 +0200

    Now returns a PyTorch tensor with correct dimensions

--- fe.py
+++ fe.py
@@ -3,7 +3,7 @@ import os
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
-
+import torch
 
 
 
@@ -16,36 +16,34 @@ def windows(data, window_size):
 
 def extract_features(fn='test.wav'):
 #Takes a wav file and returns a 2x128x128 tensor ready for uploading into AI
-    bands = 128
-    frames = 128
-    window_size = 512 * (frames - 1)
-    log_specgrams = []
-    clip,s = librosa.load(fn)
-    sound_clip = clip
-    if len(sound_clip)<88200:
-        sound_clip = np.pad(sound_clip,(0,88200-len(sound_clip)),'constant') #Pad with zeroes to the universal length
-    print('Len:',len(sound_clip))
-    print('sound_clip',sound_clip,'with shape',sound_clip.shape,'s',s)
-
-    for (start,end) in windows(sound_clip,window_size):
-       print('start:',int(start),'end',int(end))
-       s = int(start)
-       e = int(end)
-       if(len(sound_clip[s:e]) == window_size):
-           signal = sound_clip[s:e]
-           print(signal)
-           melspec = librosa.feature.melspectrogram(signal, n_mels = bands)
-           logspec = librosa.amplitude_to_db(melspec)
-           logspec = logspec.T.flatten()[:, np.newaxis].T
-           log_specgrams.append(logspec)
-    log_specgrams = np.asarray(log_specgrams)
-    log_specgrams = np.asarray(log_specgrams).reshape(len(log_specgrams),bands,frames,1)
-    features = np.concatenate((log_specgrams, np.zeros(np.shape(log_specgrams))), axis = 3)
-    for i in range(len(features)):
-        features[i, :, :, 1] = librosa.feature.delta(features[i, :, :, 0])
-
-    features = np.array(features)
-    return torch.from_numpy(features)
+        bands = 128
+        frames = 128
+        window_size = 512 * (frames - 1)
+        log_specgrams = []
+        
+        sound_clip,s = librosa.load(fn)
+        if len(sound_clip)<88200:
+            sound_clip = np.pad(sound_clip,(0,88200-len(sound_clip)),'constant') #Pad with zeroes to the universal length
+        
+        for (start,end) in windows(sound_clip,window_size):
+            s = int(start)
+            e = int(end)
+            if(len(sound_clip[s:e]) == window_size):
+                signal = sound_clip[s:e]
+                melspec = librosa.feature.melspectrogram(signal, n_mels = bands)
+                logspec = librosa.amplitude_to_db(melspec)
+                logspec = logspec.T.flatten()[:, np.newaxis].T
+                log_specgrams.append(logspec)
+        log_specgrams = np.asarray(log_specgrams)
+        log_specgrams = np.asarray(log_specgrams).reshape(len(log_specgrams),bands,frames,1)
+        features = np.concatenate((log_specgrams, np.zeros(np.shape(log_specgrams))), axis = 3)
+        for i in range(len(features)):
+            features[i, :, :, 1] = librosa.feature.delta(features[i, :, :, 0])
+        features = np.array(features)
+        features = np.swapaxes(features,1,3) #channels go first so we need to swap axis 1 and 3
+        sample = torch.from_numpy(features)
+        sample = sample.squeeze(0)
+        return sample
 
 
 
